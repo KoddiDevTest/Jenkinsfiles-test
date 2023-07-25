40e00a2c7dfe33791091dcd006303b4dde26ecb0
Kat Gao <kat.gao@koddi.com>
2023-07-25 10:31:57 -0500

    wahoo

--- scripts/verify-feature-flags/verify-feature-flags.js
+++ scripts/verify-feature-flags/verify-feature-flags.js
@@ -8,6 +8,7 @@ const envToCheck = process.env.ENV_TO_CHECK;
 const envToCheckAgainst = process.env.ENV_TO_CHECK_AGAINST;
 
 const mismatchedFeatureFlags = [];
+const environments = {};
 
 const getAndCompareFeatureFlags = async () => {
   const response = await axios
@@ -36,33 +37,47 @@ const getAndCompareFeatureFlags = async () => {
   }
 };
 
-const printEnvironmentNames = (env) => {
-  switch (env) {
-    case "development":
-      return "Development";
-    case "test":
-      return "Staging";
-    case "production":
-      return "Production";
-    default:
-      return "Unknown";
+const getAndParseAllEnvironments = async () => {
+  const response = await axios
+    .get(
+      `https://app.launchdarkly.com/api/v2/projects/${projectKey}/environments`,
+      {
+        headers: {
+          Authorization: `${apiKey}`,
+        },
+      }
+    )
+    .catch((error) => {
+      console.log(error);
+      process.exit(1);
+    });
+
+  const environmentData = response.data.items;
+
+  for (environment of environmentData) {
+    environments[environment.key] = environment.name;
   }
+};
+
+const getEnvName = (env) => {
+  return environments[env];
 };
 
 const main = async () => {
   await getAndCompareFeatureFlags();
+  await getAndParseAllEnvironments();
 
   if (mismatchedFeatureFlags.length) {
     console.log(
-      `Mismatched feature flags for ${printEnvironmentNames(
+      `Mismatched feature flags for ${getEnvName(
         envToCheck
-      )} against ${printEnvironmentNames(envToCheckAgainst)}:`
+      )} against ${getEnvName(envToCheckAgainst)}:`
     );
     for (featureFlag of mismatchedFeatureFlags) {
       console.log(
-        `${featureFlag.key}: ${printEnvironmentNames(envToCheck)}-${
+        `${featureFlag.key}: ${getEnvName(envToCheck)}-${
           featureFlag.environments[envToCheck].on
-        }, ${printEnvironmentNames(envToCheckAgainst)}-${
+        }, ${getEnvName(envToCheckAgainst)}-${
           featureFlag.environments[envToCheckAgainst].on
         }`
       );
@@ -70,9 +85,9 @@ const main = async () => {
     process.exit(1);
   } else {
     console.log(
-      `All feature flags match for ${printEnvironmentNames(
+      `All feature flags match for ${getEnvName(
         envToCheck
-      )} against ${printEnvironmentNames(envToCheckAgainst)}:`
+      )} against ${getEnvName(envToCheckAgainst)}!`
     );
   }
 };
